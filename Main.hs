cd696504418a683a410e094c2b1d583389dbb6cd
Gabor Greif <ggreif@gmail.com>
2015-07-11 01:55:13 +0200

    about to serve MathML

--- Main.hs
+++ Main.hs
@@ -9,8 +9,22 @@ import Servant
 import Servant.HTML.Lucid
 import Lucid.Base
 import Lucid.Html5
+import Lucid.MathML
 
 import Data.Text.Lazy
+
+data MathML = MathML `Times` MathML
+
+instance Num MathML where
+  (*) = Times
+
+instance ToHtml MathML where
+  toHtml ml = table_ [rows_ "2"]
+                     (tr_ $ do td_ [class_ "top", colspan_ "2", style_ "color:red"]
+                                   (p_ "Hello, attributes!")
+                               td_ "yay!")
+              >> toHtml ("hhH" :: Text)
+
 
 instance ToHtml Int where
   toHtml = h1_ . p_ . toHtml . show
@@ -23,12 +37,14 @@ instance (ToHtml (Input t), ToHtml (Input u)) => ToHtml (Input t, Input u) where
   toHtml (t, u) = toHtml t >> toHtml u
 
 type NumberAPI = "obtainnumber" :> Get '[HTML] Int
+            :<|> "math" :> Get '[HTML] MathML
             :<|> "form" :> Get '[HTML] (Input Int)
             :<|> "formPair" :> Get '[HTML] (Input Int, Input Int)
             :<|> "add" :> Capture "x" Int :> Capture "x" Int :> Get '[HTML] Int
 
 serveNumber :: Server NumberAPI
 serveNumber =    return 42
+            :<|> (return $ 1 * 1)
             :<|> (return $ Input 25)
             :<|> (return (Input 25, Input 42))
             :<|> (\ x y -> return (x + y))
