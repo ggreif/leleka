0d5c442792b17f9a64b8483e61322aa1fa09d044
Gabor Greif <ggreif@gmail.com>
2015-07-11 01:32:20 +0200

    make formpairs working

--- Main.hs
+++ Main.hs
@@ -1,40 +1,38 @@
 {-# LANGUAGE DataKinds, TypeOperators, TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts #-}
 
 module Main where
 
 import Network.Wai.Handler.Warp
 import Servant
---import Servant.Docs
 import Servant.HTML.Lucid
 import Lucid.Base
 import Lucid.Html5
+
+import Data.Text.Lazy
 
 instance ToHtml Int where
   toHtml = h1_ . p_ . toHtml . show
 
 newtype Input t = Input t
-instance ToHtml (Input t) where
-  toHtml (Input t) = input_ [makeAttribute "type" "text", makeAttribute "name" "firstname", makeAttribute "value" "John"]
+instance Show t => ToHtml (Input t) where
+  toHtml (Input t) = input_ [makeAttribute "type" "text", makeAttribute "name" "firstname", makeAttribute "value" $ toStrict $ renderText $ toHtml $ show t]
 
 instance (ToHtml (Input t), ToHtml (Input u)) => ToHtml (Input t, Input u) where
-  toHtml (t, u) = toHtml t <> toHtml u
+  toHtml (t, u) = toHtml t >> toHtml u
 
 type NumberAPI = "obtainnumber" :> Get '[HTML] Int
             :<|> "form" :> Get '[HTML] (Input Int)
+            :<|> "formPair" :> Get '[HTML] (Input Int, Input Int)
             :<|> "add" :> Capture "x" Int :> Capture "x" Int :> Get '[HTML] Int
 
 serveNumber :: Server NumberAPI
 serveNumber =    return 42
             :<|> (return $ Input 25)
+            :<|> (return (Input 25, Input 42))
             :<|> (\ x y -> return (x + y))
 
---instance ToCapture (Capture "x" Int) where
---  toCapture _ = DocCapture "x" "an integer to add"
-
---instance ToSample Int where
---  toSample = Just 0
 
 main :: IO ()
 main = do
